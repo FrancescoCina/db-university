9477cb4e411e1ec2a735dc25913d8283709a8938
FrancescoCina <francesco.cina04@gmail.com>
2021-09-28 15:51:54 +0200

    Fatto ex-2

--- data.sql
+++ data.sql
@@ -3,3 +3,12 @@
 SELECT `name`, `surname`, `date_of_birth` 
 FROM `students` 
 WHERE YEAR(`date_of_birth`) = 1990;
+
+
+--------------------------------------------
+
+-- 2. Selezionare tutti i corsi che valgono più di 10 crediti
+
+SELECT `id`,`name`,`cfu`
+FROM `courses`
+WHERE `cfu` >= 10;
