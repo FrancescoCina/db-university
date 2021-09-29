a24816893e50b0b37c8489e94554a137f550883e
FrancescoCina <francesco.cina04@gmail.com>
2021-09-29 16:27:24 +0200

    Fatto ex-6

--- queries-join.sql
+++ queries-join.sql
@@ -53,7 +53,19 @@ JOIN `teachers`
 ON `course_teacher`.`teacher_id` = `teachers`.`id`;
 
 ---------------------------------------------------------------------------
+-- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica
 
+SELECT `teachers`.`name`,`teachers`.`surname`, `departments`.`name`, `departments`.`id`
+FROM `teachers`
+JOIN `course_teacher`
+ON `course_teacher`.`teacher_id` = `teachers`.`id`
+JOIN `courses`
+ON `courses`.`id` = `course_teacher`.`course_id`
+JOIN `degrees`
+ON `courses`.`degree_id` = `degrees`.`id`
+JOIN `departments`
+ON `degrees`.`department_id`= `departments`.`id`
+WHERE `departments`.`name` = 'Dipartimento di Matematica';
 
 ---------------------------------------------------------------------------
 
