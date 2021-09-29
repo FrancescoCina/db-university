3145d8ee1d34e7bea2983f741c85bd131a0864ff
FrancescoCina <francesco.cina04@gmail.com>
2021-09-29 15:50:49 +0200

    Fatto ex-3

--- queries-join.sql
+++ queries-join.sql
@@ -10,15 +10,23 @@ WHERE `degrees`.`name` = 'Corso di Laurea in Economia';
 ---------------------------------------------------------------------------
 -- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
 
-
 SELECT `degrees`.`name`, `departments`.`name` 
 FROM `degrees`
 JOIN `departments`
 ON `departments`.`id` = `degrees`.`department_id`
 WHERE `departments`.`name` = 'Dipartimento di Neuroscienze';
 
-
 ---------------------------------------------------------------------------
+-- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato
+
+SELECT `courses`.`name` 
+FROM `courses`
+JOIN `course_teacher`
+ON `courses`.`id` = `course_teacher`.`course_id`
+JOIN `teachers`
+ON `course_teacher`.`teacher_id` = `teachers`.`id`
+WHERE `teachers`.`name` = 'Fulvio'
+AND `teachers`.`surname` = 'Amato';
 
 ---------------------------------------------------------------------------
 
