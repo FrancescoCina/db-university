40c6a7c2f72a9d68352d3bdb49806535ea5db914
FrancescoCina <francesco.cina04@gmail.com>
2021-09-29 16:10:47 +0200

    Fatto ex-4

--- queries-join.sql
+++ queries-join.sql
@@ -29,6 +29,16 @@ WHERE `teachers`.`name` = 'Fulvio'
 AND `teachers`.`surname` = 'Amato';
 
 ---------------------------------------------------------------------------
+-- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
+
+SELECT `students`.`surname`, `students`.`name`, `degrees`.* , `departments`.`name`
+FROM `students`
+JOIN `degrees`
+ON `students`.`degree_id` = `degrees`.`id`
+JOIN `departments`
+ON `degrees`.`department_id` = `departments`.`id`
+GROUP BY `students`.`id`
+ORDER BY `students`.`surname` ASC;
 
 ---------------------------------------------------------------------------
 
