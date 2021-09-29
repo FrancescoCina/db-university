04f8b3458587228eb134534a51bb1a2cc54adda4
FrancescoCina <francesco.cina04@gmail.com>
2021-09-29 15:44:03 +0200

    Fatto ex-2

--- queries-join.sql
+++ queries-join.sql
@@ -8,6 +8,15 @@ WHERE `degrees`.`name` = 'Corso di Laurea in Economia';
 
 
 ---------------------------------------------------------------------------
+-- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
+
+
+SELECT `degrees`.`name`, `departments`.`name` 
+FROM `degrees`
+JOIN `departments`
+ON `departments`.`id` = `degrees`.`department_id`
+WHERE `departments`.`name` = 'Dipartimento di Neuroscienze';
+
 
 ---------------------------------------------------------------------------
 
@@ -22,7 +31,6 @@ WHERE `degrees`.`name` = 'Corso di Laurea in Economia';
 ---------------------------------------------------------------------------
 
 ---------------------------------------------------------------------------
-
 ---------------------------------------------------------------------------
 
 ---------------------------------------------------------------------------
